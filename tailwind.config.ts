e24eb9faf19773390c95a84fdad39c8c4448c2ae
GEE <gm.walker2022@gmail.com>
2023-11-28 19:16:17 -0500

    <ch> Project Housekeeping
    - Package & Config Files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  daisyui:{
+    themes: ["corporate", "forest"],
+  },
+  plugins: [require("daisyui")],
 }
 export default config
